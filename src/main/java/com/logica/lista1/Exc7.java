be72cb51b1c077443859828410c3d79963a221df
rafasalmeron <rafasalmeronjr@gmail.com>
2024-08-31 12:59:24 -0300

    exc 7

--- src/main/java/com/logica/lista1/Exc7.java
+++ src/main/java/com/logica/lista1/Exc7.java
@@ -4,6 +4,19 @@ package com.logica.lista1;
 //aluno e calcule a média aritmética dessas notas.
 //
 
+import java.util.Scanner;
 
 public class Exc7 {
+    public static void main(String[] args) {
+        double nota1, nota2, nota3, nota4, media;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite as 4 notas: ");
+        nota1 = entrada.nextDouble();
+        nota2 = entrada.nextDouble();
+        nota3 = entrada.nextDouble();
+        nota4 = entrada.nextDouble();
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A média das notas é: " + media);
+    }
 }
