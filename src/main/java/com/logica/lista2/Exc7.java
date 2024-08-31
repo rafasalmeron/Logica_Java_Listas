042e4c7a33a55704dd889de68ea9c75bfc7bea1d
rafasalmeron <rafasalmeronjr@gmail.com>
2024-08-31 16:43:15 -0300

    lista 2 exc7

--- src/main/java/com/logica/lista2/Exc7.java
+++ src/main/java/com/logica/lista2/Exc7.java
@@ -4,5 +4,22 @@ package com.logica.lista2;
 //um determinado número informado pelo usuário é divisível por X (Que também
 //deve ser informado pelo usuário)
 
+import java.util.Scanner;
+
 public class Exc7 {
+    public static void main(String[] args) {
+        int dividendo, divisor;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o dividendo: ");
+        dividendo = sc.nextInt();
+        System.out.println("Digite o divisor: ");
+        divisor = sc.nextInt();
+
+        if (dividendo % divisor == 0) {
+            System.out.println(dividendo+" É divisível por "+divisor);
+        }else {
+            System.out.println(dividendo+" NÃO é divisível por "+divisor);
+        }
+    }
 }
