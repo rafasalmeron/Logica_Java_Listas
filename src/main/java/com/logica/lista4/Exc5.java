b18431c4c35230d30093874bb836516900422dd9
rafasalmeron <rafasalmeronjr@gmail.com>
2024-09-04 02:51:09 -0300

    Atualizando... exc4 Lista 4

--- src/main/java/com/logica/lista4/Exc5.java
+++ src/main/java/com/logica/lista4/Exc5.java
@@ -1,4 +1,6 @@
 package com.logica.lista4;
+import java.util.Scanner;
+
 public class Exc5 {
     public static void main(String[] args) {
         System.out.println("""
@@ -8,5 +10,40 @@ public class Exc5 {
                 //um item ou ver a lista.
                 
                 """);
+        String[] lista_de_compras = new String[10];
+        int option;
+        String item;
+        Scanner entrada = new Scanner(System.in);
+
+        do{
+            System.out.println("""
+                    Menu Lista de compras:
+                    
+                    Digite 1 para inserir um item.
+                    Digite 2 ver a lista.
+                    Digite 0 para sair.
+                    """);
+            option = entrada.nextInt();
+            switch(option){
+                case 1 -> {
+                    for (int i = 0; i < lista_de_compras.length; i++) {
+                        if (lista_de_compras[i] == null){
+                            System.out.println("Descreva o item para adicionar a lista:");
+                            lista_de_compras[i] = item = entrada.next();
+                            break;
+                        }
+                    }
+                }
+                case 2 -> {
+                    for (int i = 0; true;) {
+                        while(lista_de_compras[i] != null){
+                            System.out.println(lista_de_compras[i]);
+                            i++;
+                        }break;
+                    }
+                }
+                case 0 -> System.out.println("Fechando lista....");
+            }
+        }while(option != 0);
     }
 }
--- src/main/java/com/logica/lista4/Exc6.java
+++ src/main/java/com/logica/lista4/Exc6.java
@@ -1,13 +1,13 @@
 package com.logica.lista4;
 
-//6) Aproveite a questão anterior e adiciona a opção do usuário remover
-//um item.
+
 
 public class Exc6 {
     public static void main(String[] args) {
         System.out.println("""
                 
-                
+                6) Aproveite a questão anterior e adicionar a opção do usuário remover
+                um item.
                 
                 """);
     }
